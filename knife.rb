a2d6d7eeb732140757414830f165fc28837cb4e3
narkaTee <narkaTee@users.noreply.github.com>
2016-10-07 11:59:52 +0200

    Fix the knife.rb for kitchen

--- knife.rb
+++ knife.rb
@@ -1,3 +1,5 @@
+require 'chef'
+
 CHEF_ENV = ENV['CHEF_ENV'] || "default" unless defined? CHEF_ENV
 CHEF_ENV_KNIFE = File.expand_path("../#{CHEF_ENV}/knife.rb", __FILE__) unless defined? CHEF_ENV_KNIFE
 
@@ -9,7 +11,7 @@ knife[:vault_mode] = 'client'
 trusted_certs_dir File.expand_path("~/.chef/trusted_certs")
 
 if ::File.exist?(CHEF_ENV_KNIFE)
-    Chef::Config.from_file(CHEF_ENV_KNIFE)
+    ::Chef::Config.from_file(CHEF_ENV_KNIFE)
 else
     raise ArgumentError, "The CHEF_ENV's '#{CHEF_ENV}' config file '#{CHEF_ENV_KNIFE}' could not be found! "
 end
